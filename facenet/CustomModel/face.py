c525ea251c5a9c4a140782e07a82b0772c3ca62f
Dhruv Patel <30570613+Dhruv2012@users.noreply.github.com>
2019-06-27 02:03:23 +0530

    face align new fn

--- facenet/CustomModel/face.py
+++ facenet/CustomModel/face.py
@@ -6,12 +6,15 @@ import dlib
 from imutils import face_utils
 from imutils.face_utils import FaceAligner
 import glob
+from align import *
 
 
 detector = dlib.get_frontal_face_detector()
 shape_predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 face_aligner = FaceAligner(shape_predictor, desiredFaceWidth=200)
 cnn_detector = dlib.cnn_face_detection_model_v1("mmod_human_face_detector.dat")
+alignment = AlignDlib('D:\Summer Intern 2019\FACENET\CustomModel\shape_predictor_68_face_landmarks.dat')
+
 FACE_DIR = "images(face cropped)/"
 src0 = "D:\Summer Intern 2019\FACENET\CustomModel\images"
 dest0 = "D:\Summer Intern 2019\FACENET\CustomModel\images(face cropped)"
@@ -86,7 +89,9 @@ y=0
 w=0
 h=0
 
-
+def align_mod(img, bb):
+    return alignment.align(96, img, bb,
+                           landmarkIndices=AlignDlib.OUTER_EYES_AND_NOSE)
 
 
 def cropalign():
@@ -216,6 +221,94 @@ def cropalign_cnn(src,dest,upscale):
 
 
 
+
+
+def cropalign_cnn_alignfix(src,dest,upscale):
+    create_folder(dest)  # For single image per class
+    for file in glob.glob(src + "\*.jpg" ):
+        (dirname, filename) = os.path.split(file)
+        print(filename)
+        img = cv2.imread(dirname + '/' + filename, 1)
+        j =0
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = cnn_detector(img_gray, upscale)
+        print(len(faces))
+        for i in faces:
+            j = j + 1
+            x = i.rect.left()
+            y = i.rect.top()
+            w = i.rect.right() - x
+            h = i.rect.bottom() - y
+            face_img = align_mod(img,i.rect)
+            print(face_img.shape)
+            cv2.imshow("aligned", face_img)
+            face_img = cv2.resize(face_img, (96, 96))
+            if len(faces) ==1:
+                path = dest + "/" + (filename)
+            elif len(faces1)>1:
+                path = dest + "/" + str(j) + (filename)
+            cv2.imwrite(path, face_img)
+            j= j+ 1
+
+    for file1 in glob.glob(src + "\*.png"):
+        (dirname1, filename1) = os.path.split(file1)
+        print(filename1)
+        img1 = cv2.imread(dirname1 + '/' + filename1, 1)
+        j = 0
+        img_gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+        faces1 = cnn_detector(img_gray1, upscale)
+        print(len(faces1))
+        for i in faces1:
+            j = j + 1
+            x = i.rect.left()
+            y = i.rect.top()
+            w = i.rect.right() - x
+            h = i.rect.bottom() - y
+            face_img1 = align_mod(img1,rect)
+            print(face_img1.shape)
+            cv2.imshow("aligned", face_img1)
+            face_img1 = cv2.resize(face_img1, (96, 96))
+            if len(faces) ==1:
+                path1 = dest + "/" + (filename1)
+            elif len(faces1)>1:
+                path1 = dest + "/" + str(j) + (filename1)
+            cv2.imwrite(path1, face_img1)
+            j = j + 1
+
+cropalign_cnn_alignfix(test_1,"D:\Summer Intern 2019\FACENET/testing/test_alignfix\P17EC001",1)
+cropalign_cnn_alignfix(test_2,"D:\Summer Intern 2019\FACENET/testing/test_alignfix\P17EC002",1)
+cropalign_cnn_alignfix(test_3,"D:\Summer Intern 2019\FACENET/testing/test_alignfix\P17EC003",1)
+cropalign_cnn_alignfix(test_4,"D:\Summer Intern 2019\FACENET/testing/test_alignfix\P17EC004",1)
+cropalign_cnn_alignfix(test_5,"D:\Summer Intern 2019\FACENET/testing/test_alignfix\P17EC005",1)
+cropalign_cnn_alignfix(test_6,"D:\Summer Intern 2019\FACENET/testing/test_alignfix\P17EC006",1)
+cropalign_cnn_alignfix(test_7,"D:\Summer Intern 2019\FACENET/testing/test_alignfix\P17EC007",1)
+cropalign_cnn_alignfix(test_8,"D:\Summer Intern 2019\FACENET/testing/test_alignfix\P17EC008",1)
+cropalign_cnn_alignfix(test_9,"D:\Summer Intern 2019\FACENET/testing/test_alignfix\P17EC009",1)
+cropalign_cnn_alignfix(test_10,"D:\Summer Intern 2019\FACENET/testing/test_alignfix\P17EC010",1)
+cropalign_cnn_alignfix(test_11,"D:\Summer Intern 2019\FACENET/testing/test_alignfix\P17EC011",1)
+cropalign_cnn_alignfix(test_12,"D:\Summer Intern 2019\FACENET/testing/test_alignfix\P17EC012",1)
+cropalign_cnn_alignfix(test_13,"D:\Summer Intern 2019\FACENET/testing/test_alignfix\P17EC013",1)
+cropalign_cnn_alignfix(test_14,"D:\Summer Intern 2019\FACENET/testing/test_alignfix\P17EC014",1)
+cropalign_cnn_alignfix(test_15,"D:\Summer Intern 2019\FACENET/testing/test_alignfix\P17EC015",1)
+cropalign_cnn_alignfix(test_17,"D:\Summer Intern 2019\FACENET/testing/test_alignfix\P17EC017",1)
+
+
+cropalign_cnn_alignfix(src1,"D:\Summer Intern 2019\FACENET/testing/train_alignfix\P17EC001",1)
+cropalign_cnn_alignfix(src2,"D:\Summer Intern 2019\FACENET/testing/train_alignfix\P17EC002",1)
+cropalign_cnn_alignfix(src3,"D:\Summer Intern 2019\FACENET/testing/train_alignfix\P17EC003",1)
+cropalign_cnn_alignfix(src4,"D:\Summer Intern 2019\FACENET/testing/train_alignfix\P17EC004",1)
+cropalign_cnn_alignfix(src5,"D:\Summer Intern 2019\FACENET/testing/train_alignfix\P17EC005",1)
+cropalign_cnn_alignfix(src6,"D:\Summer Intern 2019\FACENET/testing/train_alignfix\P17EC006",1)
+cropalign_cnn_alignfix(src7,"D:\Summer Intern 2019\FACENET/testing/train_alignfix\P17EC007",1)
+cropalign_cnn_alignfix(src8,"D:\Summer Intern 2019\FACENET/testing/train_alignfix\P17EC008",1)
+cropalign_cnn_alignfix(src9,"D:\Summer Intern 2019\FACENET/testing/train_alignfix\P17EC009",1)
+cropalign_cnn_alignfix(src10,"D:\Summer Intern 2019\FACENET/testing/train_alignfix\P17EC010",1)
+cropalign_cnn_alignfix(src11,"D:\Summer Intern 2019\FACENET/testing/train_alignfix\P17EC011",1)
+cropalign_cnn_alignfix(src12,"D:\Summer Intern 2019\FACENET/testing/train_alignfix\P17EC012",1)
+cropalign_cnn_alignfix(src13,"D:\Summer Intern 2019\FACENET/testing/train_alignfix\P17EC013",1)
+cropalign_cnn_alignfix(src14,"D:\Summer Intern 2019\FACENET/testing/train_alignfix\P17EC014",1)
+cropalign_cnn_alignfix(src15,"D:\Summer Intern 2019\FACENET/testing/train_alignfix\P17EC015",1)
+cropalign_cnn_alignfix(src17,"D:\Summer Intern 2019\FACENET/testing/train_alignfix\P17EC017",1)
 #cropalign_cnn(src0,dest0,1)
 '''
 cropalign_cnn(test_1,"D:\Summer Intern 2019\FACENET/testing/test_alignedv1\P17EC001",1)
