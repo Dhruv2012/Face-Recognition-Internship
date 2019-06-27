0c7a8d4f5f3cb15258dbf5de1a12cb99b24fb8af
Dhruv Patel <30570613+Dhruv2012@users.noreply.github.com>
2019-06-27 13:17:33 +0530

    files updated.....

--- facenet/CustomModel/face.py
+++ facenet/CustomModel/face.py
@@ -13,67 +13,67 @@ detector = dlib.get_frontal_face_detector()
 shape_predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 face_aligner = FaceAligner(shape_predictor, desiredFaceWidth=200)
 cnn_detector = dlib.cnn_face_detection_model_v1("mmod_human_face_detector.dat")
-alignment = AlignDlib('D:\Summer Intern 2019\FACENET\CustomModel\shape_predictor_68_face_landmarks.dat')
+alignment = AlignDlib('/home/ml/FACENET/CustomModel/shape_predictor_68_face_landmarks.dat')
 
 FACE_DIR = "images(face cropped)/"
-src0 = "D:\Summer Intern 2019\FACENET\CustomModel\images"
-dest0 = "D:\Summer Intern 2019\FACENET\CustomModel\images(face cropped)"
+src0 = "/home/ml/FACENET/CustomModel/images"
+dest0 = "/home/ml/FACENET/CustomModel/images(face cropped)"
 ######################## TRAIN ##############################
 
 
-src1 = "D:\Summer Intern 2019\FACENET/testing/train\P17EC001"
-src2 = "D:\Summer Intern 2019\FACENET/testing/train\P17EC002"
-src3 = "D:\Summer Intern 2019\FACENET/testing/train\P17EC003"
-src4 = "D:\Summer Intern 2019\FACENET/testing/train\P17EC004"
-src5 = "D:\Summer Intern 2019\FACENET/testing/train\P17EC005"
-src6 = "D:\Summer Intern 2019\FACENET/testing/train\P17EC006"
-src7 = "D:\Summer Intern 2019\FACENET/testing/train\P17EC007"
-src8 = "D:\Summer Intern 2019\FACENET/testing/train\P17EC008"
-src9 = "D:\Summer Intern 2019\FACENET/testing/train\P17EC009"
-src10 = "D:\Summer Intern 2019\FACENET/testing/train\P17EC010"
-src11 = "D:\Summer Intern 2019\FACENET/testing/train\P17EC011"
-src12 = "D:\Summer Intern 2019\FACENET/testing/train\P17EC012"
-src13 = "D:\Summer Intern 2019\FACENET/testing/train\P17EC013"
-src14 = "D:\Summer Intern 2019\FACENET/testing/train\P17EC014"
-src15 = "D:\Summer Intern 2019\FACENET/testing/train\P17EC015"
-src17 = "D:\Summer Intern 2019\FACENET/testing/train\P17EC017"
+src1 = "/home/ml/FACENET/testing/train/P17EC001"
+src2 = "/home/ml/FACENET/testing/train/P17EC002"
+src3 = "/home/ml/FACENET/testing/train/P17EC003"
+src4 = "/home/ml/FACENET/testing/train/P17EC004"
+src5 = "/home/ml/FACENET/testing/train/P17EC005"
+src6 = "/home/ml/FACENET/testing/train/P17EC006"
+src7 = "/home/ml/FACENET/testing/train/P17EC007"
+src8 = "/home/ml/FACENET/testing/train/P17EC008"
+src9 = "/home/ml/FACENET/testing/train/P17EC009"
+src10 = "/home/ml/FACENET/testing/train/P17EC010"
+src11 = "/home/ml/FACENET/testing/train/P17EC011"
+src12 = "/home/ml/FACENET/testing/train/P17EC012"
+src13 = "/home/ml/FACENET/testing/train/P17EC013"
+src14 = "/home/ml/FACENET/testing/train/P17EC014"
+src15 = "/home/ml/FACENET/testing/train/P17EC015"
+src17 = "/home/ml/FACENET/testing/train/P17EC017"
 
-FACE_DIR1 = "D:\Summer Intern 2019\FACENET/testing/train_alignedv1\P17EC001"
-FACE_DIR2 = "D:\Summer Intern 2019\FACENET/testing/train_alignedv1\P17EC002"
-FACE_DIR3 = "D:\Summer Intern 2019\FACENET/testing/train_alignedv1\P17EC003"
-FACE_DIR4 = "D:\Summer Intern 2019\FACENET/testing/train_alignedv1\P17EC004"
-FACE_DIR5 = "D:\Summer Intern 2019\FACENET/testing/train_alignedv1\P17EC005"
-FACE_DIR6 = "D:\Summer Intern 2019\FACENET/testing/train_alignedv1\P17EC006"
-FACE_DIR7 = "D:\Summer Intern 2019\FACENET/testing/train_alignedv1\P17EC007"
-FACE_DIR8 = "D:\Summer Intern 2019\FACENET/testing/train_alignedv1\P17EC008"
-FACE_DIR9 = "D:\Summer Intern 2019\FACENET/testing/train_alignedv1\P17EC009"
-FACE_DIR10 = "D:\Summer Intern 2019\FACENET/testing/train_alignedv1\P17EC010"
-FACE_DIR11 = "D:\Summer Intern 2019\FACENET/testing/train_alignedv1\P17EC011"
-FACE_DIR12 = "D:\Summer Intern 2019\FACENET/testing/train_alignedv1\P17EC012"
-FACE_DIR13 = "D:\Summer Intern 2019\FACENET/testing/train_alignedv1\P17EC013"
-FACE_DIR14 = "D:\Summer Intern 2019\FACENET/testing/train_alignedv1\P17EC014"
-FACE_DIR15 = "D:\Summer Intern 2019\FACENET/testing/train_alignedv1\P17EC015"
-FACE_DIR17 = "D:\Summer Intern 2019\FACENET/testing/train_alignedv1\P17EC017"
+FACE_DIR1 = "/home/ml/FACENET/testing/train_alignedv1/P17EC001"
+FACE_DIR2 = "/home/ml/FACENET/testing/train_alignedv1/P17EC002"
+FACE_DIR3 = "/home/ml/FACENET/testing/train_alignedv1/P17EC003"
+FACE_DIR4 = "/home/ml/FACENET/testing/train_alignedv1/P17EC004"
+FACE_DIR5 = "/home/ml/FACENET/testing/train_alignedv1/P17EC005"
+FACE_DIR6 = "/home/ml/FACENET/testing/train_alignedv1/P17EC006"
+FACE_DIR7 = "/home/ml/FACENET/testing/train_alignedv1/P17EC007"
+FACE_DIR8 = "/home/ml/FACENET/testing/train_alignedv1/P17EC008"
+FACE_DIR9 = "/home/ml/FACENET/testing/train_alignedv1/P17EC009"
+FACE_DIR10 = "/home/ml/FACENET/testing/train_alignedv1/P17EC010"
+FACE_DIR11 = "/home/ml/FACENET/testing/train_alignedv1/P17EC011"
+FACE_DIR12 = "/home/ml/FACENET/testing/train_alignedv1/P17EC012"
+FACE_DIR13 = "/home/ml/FACENET/testing/train_alignedv1/P17EC013"
+FACE_DIR14 = "/home/ml/FACENET/testing/train_alignedv1/P17EC014"
+FACE_DIR15 = "/home/ml/FACENET/testing/train_alignedv1/P17EC015"
+FACE_DIR17 = "/home/ml/FACENET/testing/train_alignedv1/P17EC017"
 
 
 ################## TEST ######################################
 
-test_1 = "D:\Summer Intern 2019\FACENET/testing/test\P17EC001"
-test_2 = "D:\Summer Intern 2019\FACENET/testing/test\P17EC002"
-test_3 = "D:\Summer Intern 2019\FACENET/testing/test\P17EC003"
-test_4 = "D:\Summer Intern 2019\FACENET/testing/test\P17EC004"
-test_5 = "D:\Summer Intern 2019\FACENET/testing/test\P17EC005"
-test_6 = "D:\Summer Intern 2019\FACENET/testing/test\P17EC006"
-test_7 = "D:\Summer Intern 2019\FACENET/testing/test\P17EC007"
-test_8 = "D:\Summer Intern 2019\FACENET/testing/test\P17EC008"
-test_9 = "D:\Summer Intern 2019\FACENET/testing/test\P17EC009"
-test_10 = "D:\Summer Intern 2019\FACENET/testing/test\P17EC010"
-test_11 = "D:\Summer Intern 2019\FACENET/testing/test\P17EC011"
-test_12 = "D:\Summer Intern 2019\FACENET/testing/test\P17EC012"
-test_13 = "D:\Summer Intern 2019\FACENET/testing/test\P17EC013"
-test_14 = "D:\Summer Intern 2019\FACENET/testing/test\P17EC014"
-test_15 = "D:\Summer Intern 2019\FACENET/testing/test\P17EC015"
-test_17 = "D:\Summer Intern 2019\FACENET/testing/test\P17EC017"
+test_1 = "/home/ml/FACENET/testing/test/P17EC001"
+test_2 = "/home/ml/FACENET/testing/test/P17EC002"
+test_3 = "/home/ml/FACENET/testing/test/P17EC003"
+test_4 = "/home/ml/FACENET/testing/test/P17EC004"
+test_5 = "/home/ml/FACENET/testing/test/P17EC005"
+test_6 = "/home/ml/FACENET/testing/test/P17EC006"
+test_7 = "/home/ml/FACENET/testing/test/P17EC007"
+test_8 = "/home/ml/FACENET/testing/test/P17EC008"
+test_9 = "/home/ml/FACENET/testing/test/P17EC009"
+test_10 = "/home/ml/FACENET/testing/test/P17EC010"
+test_11 = "/home/ml/FACENET/testing/test/P17EC011"
+test_12 = "/home/ml/FACENET/testing/test/P17EC012"
+test_13 = "/home/ml/FACENET/testing/test/P17EC013"
+test_14 = "/home/ml/FACENET/testing/test/P17EC014"
+test_15 = "/home/ml/FACENET/testing/test/P17EC015"
+test_17 = "/home/ml/FACENET/testing/test/P17EC017"
 
 
 
@@ -96,7 +96,7 @@ def align_mod(img, bb):
 
 def cropalign():
     create_folder(FACE_DIR)                 #For single image per class
-    for file in glob.glob("D:\Summer Intern 2019\FACENET\CustomModel\images\*.jpg"):
+    for file in glob.glob("/home/ml/FACENET/CustomModel/images/*.jpg"):
         (dirname, filename) = os.path.split(file)
         print(filename)
         img = cv2.imread(dirname + '/' +  filename, 1)
@@ -119,7 +119,7 @@ def cropalign():
 
 
 
-    for file1 in glob.glob("images\*.png"):
+    for file1 in glob.glob("images/*.png"):
         (dirname1, filename1) = os.path.split(file1)
         print(filename1)
         img1 = cv2.imread(dirname1 + '/' + filename1, 1)
@@ -140,7 +140,7 @@ def cropalign():
             path1 = dest + "/" + filename1
             cv2.imwrite(path1, face_img1)
 
-    for file2 in glob.glob("D:\Summer Intern 2019\FACENET\CustomModel\images/test_image\*.jpg"):
+    for file2 in glob.glob("/home/ml/FACENET/CustomModel/images/test_image/*.jpg"):
         (dirname2, filename2) = os.path.split(file2)
         print(filename2)
         img2 = cv2.imread(dirname2 + '/' + filename2, 1)
@@ -158,13 +158,13 @@ def cropalign():
             face_img2 = face_aligned2
             print(face_img2.shape)
             cv2.imshow("aligned", face_img2)
-            path2 = "D:\Summer Intern 2019\FACENET\CustomModel\images(face cropped)/test_image" + "/" + filename2
+            path2 = "/home/ml/FACENET/CustomModel/images(face cropped)/test_image" + "/" + filename2
             cv2.imwrite(path2, face_img2)
 
 
 def cropalign_cnn(src,dest,upscale):
     create_folder(dest)  # For single image per class
-    for file in glob.glob(src + "\*.jpg" ):
+    for file in glob.glob(src + "/*.jpg" ):
         (dirname, filename) = os.path.split(file)
         print(filename)
         img = cv2.imread(dirname + '/' + filename, 1)
@@ -192,7 +192,7 @@ def cropalign_cnn(src,dest,upscale):
                 path = dest + "/" + str(j) + (filename)
             cv2.imwrite(path, face_img)
 
-    for file1 in glob.glob(src + "\*.png"):
+    for file1 in glob.glob(src + "/*.png"):
         (dirname1, filename1) = os.path.split(file1)
         print(filename1)
         img1 = cv2.imread(dirname1 + '/' + filename1, 1)
@@ -225,7 +225,7 @@ def cropalign_cnn(src,dest,upscale):
 
 def cropalign_cnn_alignfix(src,dest,upscale):
     create_folder(dest)  # For single image per class
-    for file in glob.glob(src + "\*.jpg" ):
+    for file in glob.glob(src + "/*.jpg" ):
         (dirname, filename) = os.path.split(file)
         print(filename)
         img = cv2.imread(dirname + '/' + filename, 1)
@@ -250,7 +250,7 @@ def cropalign_cnn_alignfix(src,dest,upscale):
             cv2.imwrite(path, face_img)
             j= j+ 1
 
-    for file1 in glob.glob(src + "\*.png"):
+    for file1 in glob.glob(src + "/*.png"):
         (dirname1, filename1) = os.path.split(file1)
         print(filename1)
         img1 = cv2.imread(dirname1 + '/' + filename1, 1)
@@ -275,58 +275,58 @@ def cropalign_cnn_alignfix(src,dest,upscale):
             cv2.imwrite(path1, face_img1)
             j = j + 1
 
-cropalign_cnn_alignfix(test_1,"D:\Summer Intern 2019\FACENET/testing/test_alignfix\P17EC001",1)
-cropalign_cnn_alignfix(test_2,"D:\Summer Intern 2019\FACENET/testing/test_alignfix\P17EC002",1)
-cropalign_cnn_alignfix(test_3,"D:\Summer Intern 2019\FACENET/testing/test_alignfix\P17EC003",1)
-cropalign_cnn_alignfix(test_4,"D:\Summer Intern 2019\FACENET/testing/test_alignfix\P17EC004",1)
-cropalign_cnn_alignfix(test_5,"D:\Summer Intern 2019\FACENET/testing/test_alignfix\P17EC005",1)
-cropalign_cnn_alignfix(test_6,"D:\Summer Intern 2019\FACENET/testing/test_alignfix\P17EC006",1)
-cropalign_cnn_alignfix(test_7,"D:\Summer Intern 2019\FACENET/testing/test_alignfix\P17EC007",1)
-cropalign_cnn_alignfix(test_8,"D:\Summer Intern 2019\FACENET/testing/test_alignfix\P17EC008",1)
-cropalign_cnn_alignfix(test_9,"D:\Summer Intern 2019\FACENET/testing/test_alignfix\P17EC009",1)
-cropalign_cnn_alignfix(test_10,"D:\Summer Intern 2019\FACENET/testing/test_alignfix\P17EC010",1)
-cropalign_cnn_alignfix(test_11,"D:\Summer Intern 2019\FACENET/testing/test_alignfix\P17EC011",1)
-cropalign_cnn_alignfix(test_12,"D:\Summer Intern 2019\FACENET/testing/test_alignfix\P17EC012",1)
-cropalign_cnn_alignfix(test_13,"D:\Summer Intern 2019\FACENET/testing/test_alignfix\P17EC013",1)
-cropalign_cnn_alignfix(test_14,"D:\Summer Intern 2019\FACENET/testing/test_alignfix\P17EC014",1)
-cropalign_cnn_alignfix(test_15,"D:\Summer Intern 2019\FACENET/testing/test_alignfix\P17EC015",1)
-cropalign_cnn_alignfix(test_17,"D:\Summer Intern 2019\FACENET/testing/test_alignfix\P17EC017",1)
+cropalign_cnn_alignfix(test_1,"/home/ml/FACENET/testing/test_alignfix/P17EC001",1)
+cropalign_cnn_alignfix(test_2,"/home/ml/FACENET/testing/test_alignfix/P17EC002",1)
+cropalign_cnn_alignfix(test_3,"/home/ml/FACENET/testing/test_alignfix/P17EC003",1)
+cropalign_cnn_alignfix(test_4,"/home/ml/FACENET/testing/test_alignfix/P17EC004",1)
+cropalign_cnn_alignfix(test_5,"/home/ml/FACENET/testing/test_alignfix/P17EC005",1)
+cropalign_cnn_alignfix(test_6,"/home/ml/FACENET/testing/test_alignfix/P17EC006",1)
+cropalign_cnn_alignfix(test_7,"/home/ml/FACENET/testing/test_alignfix/P17EC007",1)
+cropalign_cnn_alignfix(test_8,"/home/ml/FACENET/testing/test_alignfix/P17EC008",1)
+cropalign_cnn_alignfix(test_9,"/home/ml/FACENET/testing/test_alignfix/P17EC009",1)
+cropalign_cnn_alignfix(test_10,"/home/ml/FACENET/testing/test_alignfix/P17EC010",1)
+cropalign_cnn_alignfix(test_11,"/home/ml/FACENET/testing/test_alignfix/P17EC011",1)
+cropalign_cnn_alignfix(test_12,"/home/ml/FACENET/testing/test_alignfix/P17EC012",1)
+cropalign_cnn_alignfix(test_13,"/home/ml/FACENET/testing/test_alignfix/P17EC013",1)
+cropalign_cnn_alignfix(test_14,"/home/ml/FACENET/testing/test_alignfix/P17EC014",1)
+cropalign_cnn_alignfix(test_15,"/home/ml/FACENET/testing/test_alignfix/P17EC015",1)
+cropalign_cnn_alignfix(test_17,"/home/ml/FACENET/testing/test_alignfix/P17EC017",1)
 
 
-cropalign_cnn_alignfix(src1,"D:\Summer Intern 2019\FACENET/testing/train_alignfix\P17EC001",1)
-cropalign_cnn_alignfix(src2,"D:\Summer Intern 2019\FACENET/testing/train_alignfix\P17EC002",1)
-cropalign_cnn_alignfix(src3,"D:\Summer Intern 2019\FACENET/testing/train_alignfix\P17EC003",1)
-cropalign_cnn_alignfix(src4,"D:\Summer Intern 2019\FACENET/testing/train_alignfix\P17EC004",1)
-cropalign_cnn_alignfix(src5,"D:\Summer Intern 2019\FACENET/testing/train_alignfix\P17EC005",1)
-cropalign_cnn_alignfix(src6,"D:\Summer Intern 2019\FACENET/testing/train_alignfix\P17EC006",1)
-cropalign_cnn_alignfix(src7,"D:\Summer Intern 2019\FACENET/testing/train_alignfix\P17EC007",1)
-cropalign_cnn_alignfix(src8,"D:\Summer Intern 2019\FACENET/testing/train_alignfix\P17EC008",1)
-cropalign_cnn_alignfix(src9,"D:\Summer Intern 2019\FACENET/testing/train_alignfix\P17EC009",1)
-cropalign_cnn_alignfix(src10,"D:\Summer Intern 2019\FACENET/testing/train_alignfix\P17EC010",1)
-cropalign_cnn_alignfix(src11,"D:\Summer Intern 2019\FACENET/testing/train_alignfix\P17EC011",1)
-cropalign_cnn_alignfix(src12,"D:\Summer Intern 2019\FACENET/testing/train_alignfix\P17EC012",1)
-cropalign_cnn_alignfix(src13,"D:\Summer Intern 2019\FACENET/testing/train_alignfix\P17EC013",1)
-cropalign_cnn_alignfix(src14,"D:\Summer Intern 2019\FACENET/testing/train_alignfix\P17EC014",1)
-cropalign_cnn_alignfix(src15,"D:\Summer Intern 2019\FACENET/testing/train_alignfix\P17EC015",1)
-cropalign_cnn_alignfix(src17,"D:\Summer Intern 2019\FACENET/testing/train_alignfix\P17EC017",1)
+cropalign_cnn_alignfix(src1,"/home/ml/FACENET/testing/train_alignfix/P17EC001",1)
+cropalign_cnn_alignfix(src2,"/home/ml/FACENET/testing/train_alignfix/P17EC002",1)
+cropalign_cnn_alignfix(src3,"/home/ml/FACENET/testing/train_alignfix/P17EC003",1)
+cropalign_cnn_alignfix(src4,"/home/ml/FACENET/testing/train_alignfix/P17EC004",1)
+cropalign_cnn_alignfix(src5,"/home/ml/FACENET/testing/train_alignfix/P17EC005",1)
+cropalign_cnn_alignfix(src6,"/home/ml/FACENET/testing/train_alignfix/P17EC006",1)
+cropalign_cnn_alignfix(src7,"/home/ml/FACENET/testing/train_alignfix/P17EC007",1)
+cropalign_cnn_alignfix(src8,"/home/ml/FACENET/testing/train_alignfix/P17EC008",1)
+cropalign_cnn_alignfix(src9,"/home/ml/FACENET/testing/train_alignfix/P17EC009",1)
+cropalign_cnn_alignfix(src10,"/home/ml/FACENET/testing/train_alignfix/P17EC010",1)
+cropalign_cnn_alignfix(src11,"/home/ml/FACENET/testing/train_alignfix/P17EC011",1)
+cropalign_cnn_alignfix(src12,"/home/ml/FACENET/testing/train_alignfix/P17EC012",1)
+cropalign_cnn_alignfix(src13,"/home/ml/FACENET/testing/train_alignfix/P17EC013",1)
+cropalign_cnn_alignfix(src14,"/home/ml/FACENET/testing/train_alignfix/P17EC014",1)
+cropalign_cnn_alignfix(src15,"/home/ml/FACENET/testing/train_alignfix/P17EC015",1)
+cropalign_cnn_alignfix(src17,"/home/ml/FACENET/testing/train_alignfix/P17EC017",1)
 #cropalign_cnn(src0,dest0,1)
 '''
-cropalign_cnn(test_1,"D:\Summer Intern 2019\FACENET/testing/test_alignedv1\P17EC001",1)
-cropalign_cnn(test_2,"D:\Summer Intern 2019\FACENET/testing/test_alignedv1\P17EC002",1)
-cropalign_cnn(test_3,"D:\Summer Intern 2019\FACENET/testing/test_alignedv1\P17EC003",1)
-cropalign_cnn(test_4,"D:\Summer Intern 2019\FACENET/testing/test_alignedv1\P17EC004",1)
-cropalign_cnn(test_5,"D:\Summer Intern 2019\FACENET/testing/test_alignedv1\P17EC005",1)
-cropalign_cnn(test_6,"D:\Summer Intern 2019\FACENET/testing/test_alignedv1\P17EC006",1)
-cropalign_cnn(test_7,"D:\Summer Intern 2019\FACENET/testing/test_alignedv1\P17EC007",1)
-cropalign_cnn(test_8,"D:\Summer Intern 2019\FACENET/testing/test_alignedv1\P17EC008",1)
-cropalign_cnn(test_9,"D:\Summer Intern 2019\FACENET/testing/test_alignedv1\P17EC009",1)
-cropalign_cnn(test_10,"D:\Summer Intern 2019\FACENET/testing/test_alignedv1\P17EC010",1)
-cropalign_cnn(test_11,"D:\Summer Intern 2019\FACENET/testing/test_alignedv1\P17EC011",1)
-cropalign_cnn(test_12,"D:\Summer Intern 2019\FACENET/testing/test_alignedv1\P17EC012",1)
-cropalign_cnn(test_13,"D:\Summer Intern 2019\FACENET/testing/test_alignedv1\P17EC013",1)
-cropalign_cnn(test_14,"D:\Summer Intern 2019\FACENET/testing/test_alignedv1\P17EC014",1)
-cropalign_cnn(test_15,"D:\Summer Intern 2019\FACENET/testing/test_alignedv1\P17EC015",1)
-cropalign_cnn(test_17,"D:\Summer Intern 2019\FACENET/testing/test_alignedv1\P17EC017",1)
+cropalign_cnn(test_1,"/home/ml/FACENET/testing/test_alignedv1/P17EC001",1)
+cropalign_cnn(test_2,"/home/ml/FACENET/testing/test_alignedv1/P17EC002",1)
+cropalign_cnn(test_3,"/home/ml/FACENET/testing/test_alignedv1/P17EC003",1)
+cropalign_cnn(test_4,"/home/ml/FACENET/testing/test_alignedv1/P17EC004",1)
+cropalign_cnn(test_5,"/home/ml/FACENET/testing/test_alignedv1/P17EC005",1)
+cropalign_cnn(test_6,"/home/ml/FACENET/testing/test_alignedv1/P17EC006",1)
+cropalign_cnn(test_7,"/home/ml/FACENET/testing/test_alignedv1/P17EC007",1)
+cropalign_cnn(test_8,"/home/ml/FACENET/testing/test_alignedv1/P17EC008",1)
+cropalign_cnn(test_9,"/home/ml/FACENET/testing/test_alignedv1/P17EC009",1)
+cropalign_cnn(test_10,"/home/ml/FACENET/testing/test_alignedv1/P17EC010",1)
+cropalign_cnn(test_11,"/home/ml/FACENET/testing/test_alignedv1/P17EC011",1)
+cropalign_cnn(test_12,"/home/ml/FACENET/testing/test_alignedv1/P17EC012",1)
+cropalign_cnn(test_13,"/home/ml/FACENET/testing/test_alignedv1/P17EC013",1)
+cropalign_cnn(test_14,"/home/ml/FACENET/testing/test_alignedv1/P17EC014",1)
+cropalign_cnn(test_15,"/home/ml/FACENET/testing/test_alignedv1/P17EC015",1)
+cropalign_cnn(test_17,"/home/ml/FACENET/testing/test_alignedv1/P17EC017",1)
 
 '''
 '''
